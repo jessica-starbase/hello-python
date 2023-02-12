00a64bd4c9021d33ed1e930b5695f060479eb413
jessica-starbase <125201668+jessica-starbase@users.noreply.github.com>
2023-02-12 11:03:25 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,32 @@
-from flask import Flask
-app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from Python!"
+import snowflake.connector
+import pandas as pdpath = "C:\\Users\\customers_excel.xlsx"
+import pandas as pd
+import pypyodbcscript = """SELECT * from DB.dbo.orders"""
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+file = pd.ExcelFile(path)
+df = pd.read_excel(file,  sheet_name = 'Sheet1')
+
+connection = pypyodbc.connect("Driver={SQL Server Native Client 11.0}; server=Server_Name; database=DB; uid=MSSQL_UserID; pwd=MSSQL_Passcode")cursor = connection.cursor()
+df_sql = pd.read_sql(script, connection )
+
+
+snowflake.connector.paramstyle='qmark'
+ctx = snowflake.connector.connect(
+user='snowflake_passcode',
+password='snowflake_passcode',
+account='snowflake_account'
+)
+cs = ctx.cursor()
+cs.execute(" DELETE FROM schema.python_import; ")
+
+for row in df.to_records(index=False):
+cs.execute(" Insert Into schema.python_import (cust_id , state)"
+    "VALUES (?, ? ) ", (str(row[0]),str(row[1])) )
+print("uploaded")
+
+for row in df_sql.to_records(index=False):
+    cs.execute(" Insert Into schema.python_import (order_id, cust_id, type, date, price, qty)"
+   "VALUES (?, ?, ?, ?, ?, ?) ", (str(row[0]),str(row[1]),str(row[2]),str(row[3]),str(row[4]),str(row[5])) )
+print("uploaded")
+
